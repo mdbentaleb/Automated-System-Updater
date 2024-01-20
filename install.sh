994736b5eee8af49f266617f4d58c385de2403dd
mdbentaleb <bentalebm@outlook.com>
2024-01-20 17:43:05 +0100

    installation

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install -y figlet
+chmod +x sysOpt.sh
+sleep 2
+clear
+echo -e "\n>>>>>>>> \e[32mdone..!\e[0m"
+sleep 1
+echo -e "-Command to Start System Updater:  \e[38;5;214m./sysOpt.sh\e[0m\n"
+sleep 1
