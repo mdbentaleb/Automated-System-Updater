8a9b1c5d5d0c6ddde78656b3ed4668d231f9d0a9
mdbentaleb <bent4leb@gmail.com>
2024-09-04 01:51:39 +0100

    config

--- config/config.sh
+++ config/config.sh
@@ -3,12 +3,8 @@
 readonly GREEN='\033[38;5;84m'
 readonly RED='\033[38;5;197m'
 readonly BLUE='\033[38;5;45m'
-readonly PURPLE='\033[38;5;63m'
 readonly PINK='\033[38;5;207m'
 readonly ORANGE='\033[38;5;214m'
 readonly GREY='\033[38;5;8m'
 readonly YELLOW='\033[1;33m'
-readonly BG_GREEN='\033[48;5;84m'
-readonly BG_RED='\033[48;5;197m'
 readonly RESET='\033[0m'
-
